21857f95d355c39fdbd048ef4eda30f964805e30
Jorge Moura <jjorge.moura@gmail.com>
2021-05-31 09:08:14 +0100

    add icon and setup project structure

---
+++ vagemcastTests/VagemcastTests.swift
@@ -0,0 +1,8 @@
+//
+//  Copyright © 2021  ___ORGANIZATIONNAME___ . All rights reserved.
+
+import XCTest
+@testable import vagemcast
+
+class VagemcastTests: XCTestCase {
+}
---
+++ vagemcastUITests/VagemcastUITests.swift
@@ -0,0 +1,7 @@
+//
+//  Copyright © 2021  ___ORGANIZATIONNAME___ . All rights reserved.
+
+import XCTest
+
+class VagemcastUITests: XCTestCase {
+}
