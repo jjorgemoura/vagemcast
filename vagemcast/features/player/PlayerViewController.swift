e3f5766e65d232e1443c11f28c7c8d7c1c934c71
Jorge Moura <jjorge.moura@gmail.com>
2021-05-31 09:15:14 +0100

    Create Player View

--- vagemcast/features/player/PlayerViewController.swift
+++ vagemcast/features/player/PlayerViewController.swift
@@ -3,7 +3,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- vagemcast/resources/SceneDelegate.swift
+++ vagemcast/resources/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             self.window = window
 
-            window.rootViewController = ViewController()
+            window.rootViewController = PlayerViewController()
 
             window.makeKeyAndVisible()
         }
