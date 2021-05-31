6519601a63d51777ddd31e1fb90f3b0d83215f3a
Jorge Moura <jjorge.moura@gmail.com>
2021-05-31 08:22:48 +0100

    init setup

---
+++ vagemcast/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2021  ___ORGANIZATIONNAME___ . All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
