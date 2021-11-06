3cc01d3dce8ad83754f047d6f00fd745cb68e52a
Jorge Moura <jjorge.moura@gmail.com>
2021-11-06 22:12:22 +0000

    reboot

---
+++ App/iOS/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2021  ___ORGANIZATIONNAME___ . All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/iOS/vagemcastApp.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2021  ___ORGANIZATIONNAME___ . All rights reserved.
+
+import SwiftUI
+
+@main
+struct vagemcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
